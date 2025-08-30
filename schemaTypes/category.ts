c9357164cc1e229a24cbd034b98fe9fb92fe43ae
alejandromc97 <amartcart@hotmail.com>
2025-08-30 17:31:56 -0600

    added images for categories

--- schemaTypes/category.ts
+++ schemaTypes/category.ts
@@ -15,6 +15,21 @@ export default defineType({
       name: 'description',
       type: 'text',
       title: 'Description'
+    },
+    {
+      name: 'mostSearched',
+      type: 'boolean',
+      title: 'Mostrar en Más Buscado',
+      description: 'Muestra esta categoría en la sección de Más buscado por categoría',
+      initialValue: false
+    },
+    {
+      name: 'image',
+      type: 'image',
+      title: 'Imagen de la categoría',
+      options: {
+        hotspot: true
+      }
     }
   ]
 })
